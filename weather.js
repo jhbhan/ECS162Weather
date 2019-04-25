22c6a4f46f8a240d2850a2fa3e8d4d10553beee2
Jeonghwan Bhan <jhbhan@gmail.com>
2019-04-24 23:57:43 -0700

    most of doppler done

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ let imageArray = []  // global variable to hold stack of images for animation
 let count = 0;          // global var
 let imageCounter = 0;
 
-//getImages();
+getImages();
 
 ////////////////////////////////////////////////
 //////////////////UPDATE HTML///////////////////
@@ -70,7 +70,6 @@ function updateTime(date, weatherClass){
     				hours = hours - 12;
   			}
 			var strTime = hours + ampm;
-			console.log("here");
 			let change_time = "time"+i;
 			let currentTime = document.getElementById(change_time).children[0];
   			currentTime.textContent = strTime;
@@ -166,58 +165,65 @@ function newRequest() {
 
 function getImages(){
 	getTenImages();
+	console.log("got ten images");
 	setInterval(setImages(),5000);
+	console.log("leaving interval");
 }
-  function setImages(){
-	  console.log("hi");
+function setImages(){
+	  console.log("in set images");
+	  console.log(imageArray[1]);
 	  var image = document.getElementById("img3");
 	  image.src = imageArray[imageCounter].src;
-	  if(imageCounter == 10){
+	  if(imageCounter == 9){
 		imageCounter = 0;
 	  }else{
 		imageCounter++;
 	  }
 }
   
-  function addToArray(newImage) {
-	  if (count < 10) {
-		  newImage.id = "doppler_"+count;
-		  newImage.style.display = "none";
-		  imageArray.push(newImage);
-		  count = count+1;
-		  if (count >= 10) {
-			  console.log("Got 10 doppler images");
-		  }
-	  }
-  }
-  
-  function tryToGetImage(dateObj) {
-	  let dateStr = dateObj.getUTCFullYear();
-	  dateStr += String(dateObj.getUTCMonth() + 1).padStart(2, '0'); //January is 0!
-	  dateStr += String(dateObj.getUTCDate()).padStart(2, '0');
-  
-	  let timeStr = String(dateObj.getUTCHours()).padStart(2,'0')
-	  timeStr += String(dateObj.getUTCMinutes()).padStart(2,'0');
-  
-	  let filename = "DAX_"+dateStr+"_"+timeStr+"_N0R.gif";
-	  let newImage = new Image();
-	  newImage.onload = function () {
-		  console.log("got image "+filename);
-		  addToArray(newImage);
-	  }
-	  newImage.onerror = function() {
-		  console.log("failed to load "+filename);
-	  }
-	  newImage.src = "http://radar.weather.gov/ridge/RadarImg/N0R/DAX/"+filename;
-  }
-  
-  
-  function getTenImages() {
-	  let dateObj = new Date();  // defaults to current date and time
-	  // if we try 150 images, and get one out of every 10, we should get enough
-	  for (let i = 0; i < 150; i++) {
-		  newImage = tryToGetImage(dateObj);
-		  dateObj.setMinutes( dateObj.getMinutes()-1 ); // back in time one minute
-	  }
-  
-  }
+
+function addToArray(newImage) {
+	if (count < 10) {
+		newImage.id = "doppler_"+count;
+		newImage.style.display = "none";
+		imageArray.push(newImage);
+		count = count+1;
+		if (count >= 10) {
+			console.log("Got 10 doppler images");
+		}
+	}
+}
+
+
+function tryToGetImage(dateObj) {
+	let dateStr = dateObj.getUTCFullYear();
+	dateStr += String(dateObj.getUTCMonth() + 1).padStart(2, '0'); //January is 0!
+	dateStr += String(dateObj.getUTCDate()).padStart(2, '0');
+
+	let timeStr = String(dateObj.getUTCHours()).padStart(2,'0');
+	timeStr += String(dateObj.getUTCMinutes()).padStart(2,'0');
+
+	let filename = "DAX_"+dateStr+"_"+timeStr+"_N0R.gif";
+	let newImage = new Image();
+	newImage.onload = function () {
+		// console.log("got image "+filename);
+		addToArray(newImage);
+	}
+	newImage.onerror = function() {
+		// console.log("failed to load "+filename);
+	}
+	newImage.src = "http://radar.weather.gov/ridge/RadarImg/N0R/DAX/"+filename;
+}
+
+
+function getTenImages() {
+	let dateObj = new Date();  // defaults to current date and time
+	// if we try 150 images, and get one out of every 10, we should get enough
+	for (let i = 0; i < 150; i++) {
+		newImage = tryToGetImage(dateObj);
+		dateObj.setMinutes( dateObj.getMinutes()-1 ); // back in time one minute
+	}
+
+}
+
+
